5cc8a3ab993c5ba820fea2dc2ef90c4f9527b803
Abhijit Hansda <abhijit786hansda@gmail.com>
2015-06-22 00:07:27 +0530

    Finding Unicodes of certain Characters

---
+++ Unicodes.java
@@ -0,0 +1,16 @@
+/* Program to find Unicode of #,$,%,6 */
+
+class Unicodes {
+    public static void main(String[] args) {
+	    char a =  '#';
+		char b = '$';
+		char c = '%';
+		char d = '6';
+		
+		System.out.println((int) a);
+		System.out.println((int) b);
+		System.out.println((int) c);
+		System.out.println((int) d);
+		
+	}
+}
