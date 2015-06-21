9089da5b64f400a40098b4fdd14b4d0962e6b4ef
Abhijit Hansda <abhijit786hansda@gmail.com>
2015-06-21 23:48:37 +0530

    Printing  "I will do my Assignment 100 times" (using do while)

---
+++ DoWhilePrint.java
@@ -0,0 +1,14 @@
+/* Program to print "I will do my Assignment 100 times" using do while */
+
+class DoWhilePrint {
+    public static void main(String[] args) {
+	    int number = 1;
+		
+		do {
+		    System.out.println ( number+" I will do my Assignment 100 times " );
+			number++;
+		}
+		while(number <= 100);
+	}
+}
+		
